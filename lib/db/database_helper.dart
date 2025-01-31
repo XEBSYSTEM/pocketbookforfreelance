fed2def9d8468cd82235612db4aff225e96545dc
HP-REB-SYSTEM\rei72 <rei.720928.fukuoka@outlook.com>
2025-01-31 16:30:18 +0900

    chore: データベースファイルの拡張子を変更
    - .dbから.sqliteに拡張子を変更
    - より標準的なSQLite拡張子を使用

--- lib/db/database_helper.dart
+++ lib/db/database_helper.dart
@@ -10,7 +10,7 @@ class DatabaseHelper {
 
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDB('pocketbook.db');
+    _database = await _initDB('pocketbook.sqlite');
     return _database!;
   }
 
