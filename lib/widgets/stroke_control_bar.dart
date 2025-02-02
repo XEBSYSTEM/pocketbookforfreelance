aa00d8bdc0dd3e80efacdfcfa5836effad1d93bb
HP-REB-SYSTEM\rei72 <rei.720928.fukuoka@outlook.com>
2025-02-02 17:48:02 +0900

    fix: StrokeControlBarのレイアウト問題を修正
    - LayoutBuilderを使用してレイアウト計算を改善
    - Scaffold.geometryOfエラーを解消
    - ペイントフェーズでの適切なジオメトリ計算を実装

--- lib/widgets/stroke_control_bar.dart
+++ lib/widgets/stroke_control_bar.dart
@@ -17,41 +17,46 @@ class StrokeControlBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ...availableStrokeWidths.map((width) {
-            return IconButton(
-              onPressed: () => onStrokeWidthChanged(width),
-              icon: Container(
-                width: 24,
-                height: 24,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color:
-                        currentStrokeWidth == width ? Colors.blue : Colors.grey,
-                    width: 2,
-                  ),
-                ),
-                child: Center(
-                  child: Container(
-                    width: width,
-                    height: width,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ...availableStrokeWidths.map((width) {
+                return IconButton(
+                  onPressed: () => onStrokeWidthChanged(width),
+                  icon: Container(
+                    width: 24,
+                    height: 24,
                     decoration: BoxDecoration(
-                      color: currentMode == DrawingMode.pen
-                          ? Colors.black
-                          : Colors.red,
                       shape: BoxShape.circle,
+                      border: Border.all(
+                        color: currentStrokeWidth == width
+                            ? Colors.blue
+                            : Colors.grey,
+                        width: 2,
+                      ),
+                    ),
+                    child: Center(
+                      child: Container(
+                        width: width,
+                        height: width,
+                        decoration: BoxDecoration(
+                          color: currentMode == DrawingMode.pen
+                              ? Colors.black
+                              : Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            );
-          }).toList(),
-        ],
-      ),
+                );
+              }).toList(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
